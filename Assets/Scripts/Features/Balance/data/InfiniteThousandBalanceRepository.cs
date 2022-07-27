dc724e3a0d93bd707e5aaf937487c2eea37fb2c1
Denis Rebrov <adm.upreality@gmail.com>
2022-07-27 19:58:35 +0300

    added stub balance repo

---
+++ Assets/Scripts/Features/Balance/data/InfiniteThousandBalanceRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Features.Balance.domain;
+using Features.Balance.domain.repositories;
+using UniRx;
+
+namespace Features.Balance.data
+{
+    public class InfiniteThousandBalanceRepository: IBalanceRepository
+    {
+        public IObservable<int> GetBalance(CurrencyType currencyType)
+        {
+            return Observable.Return(1000);
+        }
+
+        public void Add(int value, CurrencyType currencyType)
+        {
+            //do nothing
+        }
+
+        public void Remove(int value, CurrencyType currencyType)
+        {
+            //do nothing
+        }
+    }
+}
